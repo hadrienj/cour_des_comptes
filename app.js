c3db9cccd17da696996eaf94851d67d0bdbc3690
hadrienj <hadrienjean@gmail.com>
2018-04-01 15:52:59 +0200

    plot data from csv using reclinejs

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
-var dataset = new recline.Model.Dataset({
-  url: 'https://files.datapress.com/london/dataset/london-average-air-quality-levels/2018-03-23T11:58:28.61/air-quality-london-monthly-averages.csv',
+$(document).ready(function() {
+
+const path = "https://files.datapress.com/london/dataset/london-average-air-quality-levels/2018-03-23T11:58:28.61/air-quality-london-monthly-averages.csv";
+
+const dataset = new recline.Model.Dataset({
+  url: path,
   backend: 'dataproxy',
 });
-console.log(dataset);
-
 dataset.fetch();
 
-// show the data for illustrations sake
-var grid = new recline.View.SlickGrid({
+
+// Show data table
+const grid = new recline.View.SlickGrid({
   model: dataset,
-  el:  $('#mygrid'),
+  el:  $('#tableContainer'),
 });
 grid.visible = true;
+grid.render();
+
+
+// Plot data
+const graph = new recline.View.Graph({
+  model: dataset,
+  state: {
+    graphType: "lines-and-points",
+    group: "Month",
+    series: ["London Mean Background:PM10 Particulate (ug/m3)",
+      "London Mean Background:Nitrogen Dioxide (ug/m3)"]
+  }
+});
+$('#plotContainer').append(graph.el);
+graph.render();
+graph.redraw();
+
+});
